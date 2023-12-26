6a88aba7842e9a991d3168ffd591266cebf7feea
Hamad <hjuraysi.c@tetco.sa>
2023-12-26 14:26:45 +0300

    add regex

--- src/Lib/utils.ts
+++ src/Lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export const langRegex = {
+    enRegex: /^[a-zA-Z- ]+$/,
+    arRegex: /^[\u0621-\u064A- ]+$/,
+    bothArAndEnRegexonlyLetters: /^[a-zA-Z-\u0621-\u064A ]+$/,
+    enletterAndNumbersRegex: /^[a-zA-Z0-9]*$/,
+    mobileNumber: /^(009665)(\d){8}$/,
+    TelPhone: /^(0096611)(\d){7}$/,
+    nationalId: /^1(\d){9}$/,
+    nationalOrResidentId: /^[12](\d){9}$/,
+    nonSaudianNationalId: /^2(\d){9}$/,
+    tags: /<[^>]*>?/gm,
+    specialCharcter: /[!@#$%^&*()_+\-'~<>/\\?|÷×]/,
 }
