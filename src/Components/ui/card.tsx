31296d14d969461d80f8119f4a5fe2619d1f9dbc
mahmoud a.toumy <maabdullah.c@tetco.sa>
2023-12-28 15:51:48 +0200

    remove card footer padding

--- src/Components/ui/card.tsx
+++ src/Components/ui/card.tsx
@@ -28,7 +28,7 @@ const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDi
 CardContent.displayName = 'CardContent'
 
 const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
-    <div ref={ref} className={cn('flex items-center p-6 pt-0', className)} {...props} />
+    <div ref={ref} className={cn('flex items-center', className)} {...props} />
 ))
 CardFooter.displayName = 'CardFooter'
 
