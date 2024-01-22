dcfce7f19f83cdd88cffaab311df70b8c3300109
Hamad <hjuraysi.c@tetco.sa>
2024-01-22 14:11:26 +0300

    add accordion

---
+++ src/Components/ui/accordion.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable react/prop-types */
+import * as AccordionPrimitive from '@radix-ui/react-accordion'
+import * as React from 'react'
+
+import { cn } from '../../Lib/utils'
+
+const Accordion = AccordionPrimitive.Root
+
+const AccordionItem = React.forwardRef<
+    React.ElementRef<typeof AccordionPrimitive.Item>,
+    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({ className, ...props }, ref) => (
+    <AccordionPrimitive.Item
+        ref={ref}
+        className={cn('border-b border-b-border py-space-04 px-space-02 flex flex-col gap-space-03', className)}
+        {...props}
+    />
+))
+AccordionItem.displayName = 'AccordionItem'
+
+const AccordionTrigger = React.forwardRef<
+    React.ElementRef<typeof AccordionPrimitive.Trigger>,
+    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
+>(({ className, children, ...props }, ref) => (
+    <AccordionPrimitive.Header className="flex">
+        <AccordionPrimitive.Trigger
+            ref={ref}
+            className={cn(
+                'flex hover:bg-card-hover disabled:text-disabled text-subtitle-02 gap-space-03 font-IBMBold flex-1 items-center text-right justify-between transition-all group',
+                className,
+            )}
+            {...props}>
+            {children}
+        </AccordionPrimitive.Trigger>
+    </AccordionPrimitive.Header>
+))
+AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
+
+const AccordionContent = React.forwardRef<
+    React.ElementRef<typeof AccordionPrimitive.Content>,
+    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+    <AccordionPrimitive.Content
+        ref={ref}
+        className="overflow-hidden text-body-02 text-card-foreground transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+        {...props}>
+        <div className={cn('pb-4 pt-0', className)}>{children}</div>
+    </AccordionPrimitive.Content>
+))
+
+AccordionContent.displayName = AccordionPrimitive.Content.displayName
+
+export { Accordion, AccordionContent, AccordionItem, AccordionTrigger }
