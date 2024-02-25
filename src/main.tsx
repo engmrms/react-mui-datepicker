939ce7b6b88d146af7776f2a9f49f7e16e1dab15
mohammad saleh <mrabah@tetco.sa>
2024-02-25 13:36:45 +0300

    build configuration

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <h1>Tetco Design System</h1>
+    </React.StrictMode>,
+)
