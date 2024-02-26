01e6226cdf494e49b97135ac91f0708847e056b2
mohammad saleh <mrabah@tetco.sa>
2024-02-26 09:53:49 +0300

    redifine sonar function name

--- src/Lib/sonar.ts
+++ src/Lib/sonar.ts
@@ -1,21 +1,21 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const sonarqubeScanner = require("sonarqube-scanner");
+const sonarqubeScannerShared = require('sonarqube-scanner')
 
-sonarqubeScanner(
-  {
-    serverUrl: "#{sonarQubeServerUrl}#",
-    token: "#{sonarQubeToken}#",
-    options: {
-      "sonar.projectName": "#{sonarQubeProjectName}#",
-      "sonar.projectKey": "#{sonarQubeProjectKey}#",
-      "sonar.projectVersion": "#{sonarQubeProjectVersion}#",
-      "sonar.sources": "./src",
-      "sonar.exclusions": "**/*.test.*,**/__snapshots__/**,**/sonar.ts",
-      // "sonar.tests": "./src",
-      // "sonar.test.inclusions": "./src/**/*.test.tsx",
-      // "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
-      // "sonar.testExecutionReportPaths": "reports/test-report.xml",
+sonarqubeScannerShared(
+    {
+        serverUrl: '#{sonarQubeServerUrl}#',
+        token: '#{sonarQubeToken}#',
+        options: {
+            'sonar.projectName': '#{sonarQubeProjectName}#',
+            'sonar.projectKey': '#{sonarQubeProjectKey}#',
+            'sonar.projectVersion': '#{sonarQubeProjectVersion}#',
+            'sonar.sources': './src',
+            'sonar.exclusions': '**/*.test.*,**/__snapshots__/**,**/sonar.ts',
+            // "sonar.tests": "./src",
+            // "sonar.test.inclusions": "./src/**/*.test.tsx",
+            // "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
+            // "sonar.testExecutionReportPaths": "reports/test-report.xml",
+        },
     },
-  },
-  () => process.exit()
-);
+    () => process.exit(),
+)
