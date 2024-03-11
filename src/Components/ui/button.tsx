6885bebbdd573ec952ca1259df69746ed3f0a226
mohammad saleh <mrabah@tetco.sa>
2024-03-11 14:35:19 +0300

    update button disabled

--- src/Components/ui/button.tsx
+++ src/Components/ui/button.tsx
@@ -6,13 +6,13 @@ import * as React from 'react'
 import { cn } from '../../Lib/utils'
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center h-[4.8rem] text-base ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none  disabled:bg-disabled disabled:cursor-not-allowed',
+    'inline-flex items-center justify-center h-[4.8rem] text-base ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none   disabled:cursor-not-allowed ',
     {
         variants: {
             variant: {
-                default: 'font-IBMBold',
-                outline: 'border border-input',
-                ghost: ' border-none text',
+                default: 'font-IBMBold disabled:bg-disabled disabled:text-card',
+                outline: 'border border-input disabled:text-disabled',
+                ghost: ' border-none disabled:text-disabled ',
                 link: ' text-current underline-offset-4 hover:underline',
             },
             size: {
