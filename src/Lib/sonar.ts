c3ad112061f8b987084f46de9bced631107812f1
Hamad <hjuraysi.c@tetco.sa>
2024-06-12 13:36:06 +0300

    update sonarQube

--- src/Lib/sonar.ts
+++ src/Lib/sonar.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const sonarqubeScannerShared = require('sonarqube-scanner')
+const sonarqubeScannerShared = require('sonarqube-scanner').default
 
 sonarqubeScannerShared(
     {
