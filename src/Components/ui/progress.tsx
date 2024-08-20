12b50a1e5213b9526c61f259a30446c8f8a7a608
Hamad <aljuraysih@gmail.com>
2024-08-20 15:36:36 +0300

    chnage from bg to colors

--- src/Components/ui/progress.tsx
+++ src/Components/ui/progress.tsx
@@ -5,17 +5,17 @@ import * as React from 'react'
 import { cn } from '../../Lib/utils'
 
 interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
-    bg?: 'bg-primary' | 'bg-secondary' | 'bg-tertiary' | 'bg-error' | 'bg-warning' | 'bg-success' | 'bg-info'
+    colors?: 'bg-primary' | 'bg-secondary' | 'bg-tertiary' | 'bg-error' | 'bg-warning' | 'bg-success' | 'bg-info'
 }
 
 const Progress = React.forwardRef<React.ElementRef<typeof ProgressPrimitive.Root>, ProgressProps>(
-    ({ className, value, bg = 'bg-primary', ...props }, ref) => (
+    ({ className, value, colors = 'bg-primary', ...props }, ref) => (
         <ProgressPrimitive.Root
             ref={ref}
             className={cn('relative h-space-02 w-full overflow-hidden rounded-1 bg-background-secondary', className)}
             {...props}>
             <ProgressPrimitive.Indicator
-                className={cn('h-full w-full flex-1 transition-all', bg)}
+                className={cn('h-full w-full flex-1 transition-all', colors)}
                 style={{ transform: props?.dir === 'rtl' ? `translateX(${100 - (value || 0)}%)` : `translateX(-${100 - (value || 0)}%)` }}
             />
         </ProgressPrimitive.Root>
