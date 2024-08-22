cdd93a3022a0c369e17ee0176183c7e564fed064
mahmoud a.toumy <maabdullah.c@tetco.sa>
2024-08-22 13:24:13 +0300

    update hooks

--- src/Hooks/useCachedData.ts
+++ src/Hooks/useCachedData.ts
@@ -44,6 +44,7 @@ type QueryState<T> = {
  * Custom hook to retrieve cached data and its associated state.
  *
  * @template T - The type of data being queried.
+ * @param {QueryClient} queryClient - The queryClient instance.
  * @param {QueryKey} queryKey - The key used to identify the query in the cache.
  * @param {(data: T | undefined) => T | undefined} [transform] - Optional transformation function to apply to the cached data.
  * @returns {QueryState<T>} The state of the query, including the data, status, and any errors.
--- src/Hooks/useUpdateCachedData.ts
+++ src/Hooks/useUpdateCachedData.ts
@@ -1,4 +1,5 @@
-import { QueryKey, useQueryClient } from '@tanstack/react-query'
+import { QueryKey, useQueryClient } from "@tanstack/react-query";
+import { Draft, produce } from "immer";
 
 /**
  * A function that updates cached data based on the old data.
@@ -7,7 +8,7 @@ import { QueryKey, useQueryClient } from '@tanstack/react-query'
  * @param {T | undefined} oldData - The current cached data or undefined if no data exists.
  * @returns {T} - The updated data.
  */
-type UpdaterFunction<T> = (oldData: T | undefined) => T
+type UpdaterFunction<T> = (draft: Draft<NoInfer<T>> | undefined) => T;
 
 /**
  * A custom hook to update cached data in React Query.
@@ -16,22 +17,26 @@ type UpdaterFunction<T> = (oldData: T | undefined) => T
  * @returns {(key: QueryKey, updater: T | UpdaterFunction<T>) => void} - A function to update the cached data.
  */
 export const useUpdateCachedData = <T>() => {
-    const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
-    /**
-     * Updates the cached data for a specific query key.
-     *
-     * @param {QueryKey} key - The key that identifies the cached data to update.
-     * @param {T | UpdaterFunction<T>} updater - The new data or a function that returns the updated data.
-     */
-    const updateCachedData = (key: QueryKey, updater: T | UpdaterFunction<T>) => {
-        queryClient.setQueryData<T>(key, oldData => {
-            // Apply the updater function to modify the old data
-            return typeof updater === 'function' ? (updater as UpdaterFunction<T>)(oldData) : updater
-        })
-    }
+  /**
+   * Updates the cached data for a specific query key.
+   *
+   * @param {QueryKey} key - The key that identifies the cached data to update.
+   * @param {T | UpdaterFunction<T>} updater - The new data or a function that returns the updated data.
+   */
+  const updateCachedData = (key: QueryKey, updater: T | UpdaterFunction<T>) => {
+    queryClient.setQueryData<T>(key, oldData => {
+      // Apply the updater function to modify the old data
+      if (typeof updater !== "function") return updater;
 
-    return updateCachedData
-}
+      return produce(oldData, draft => {
+        (updater as UpdaterFunction<T>)(draft);
+      });
+    });
+  };
 
-export default useUpdateCachedData
+  return updateCachedData;
+};
+
+export default useUpdateCachedData;
