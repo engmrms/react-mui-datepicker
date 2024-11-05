057d6d7d6223a64d6ef245a0dc62f36cd14188de
Hamad <hjuraysi.c@tetco.sa>
2024-11-05 13:24:25 +0300

    add lang arg to dateFormatter

--- src/Lib/utils.ts
+++ src/Lib/utils.ts
@@ -95,13 +95,15 @@ export const dateFormatter = ({
     format,
     isHijri = false,
     hasHijriFormat,
+    lang = document.dir === 'rtl' ? 'ar' : 'en',
 }: {
     date: string | Date | undefined
     format: 'short' | 'mid' | 'long'
     isHijri?: boolean
     hasHijriFormat?: boolean
+    lang?: string
 }) => {
-    const isArabic = document.dir === 'rtl'
+    const isArabic = lang === 'ar'
     moment.updateLocale(isArabic ? 'ar-sa' : 'en', {})
     switch (format) {
         case 'short':
