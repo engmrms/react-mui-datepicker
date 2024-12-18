1958e3ae0462736ce2dd8bf1c54eadd6d1f4d6e8
Hamad <hjuraysi@tetco.sa>
2024-12-18 10:20:29 +0300

    add icon to progress

--- src/Components/ui/progress.tsx
+++ src/Components/ui/progress.tsx
@@ -1,24 +1,37 @@
 /* eslint-disable react/prop-types */
 import * as ProgressPrimitive from '@radix-ui/react-progress'
 import * as React from 'react'
-
 import { cn } from '../../Lib/utils'
 
 interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
     colors?: 'bg-primary' | 'bg-secondary' | 'bg-tertiary' | 'bg-error' | 'bg-warning' | 'bg-success' | 'bg-info' | 'bg-secondary-light'
+    icon?: React.ReactNode
 }
 
 const Progress = React.forwardRef<React.ElementRef<typeof ProgressPrimitive.Root>, ProgressProps>(
-    ({ className, value, colors = 'bg-primary', ...props }, ref) => (
-        <ProgressPrimitive.Root
-            ref={ref}
-            className={cn('relative h-space-02 w-full overflow-hidden rounded-1 bg-background-secondary', className)}
-            {...props}>
-            <ProgressPrimitive.Indicator
-                className={cn('h-full w-full flex-1 transition-all', colors)}
-                style={{ transform: props?.dir === 'rtl' ? `translateX(${100 - (value || 0)}%)` : `translateX(-${100 - (value || 0)}%)` }}
-            />
-        </ProgressPrimitive.Root>
+    ({ className, value, colors = 'bg-primary', icon, ...props }, ref) => (
+        <div className="relative w-full">
+            <ProgressPrimitive.Root
+                ref={ref}
+                className={cn('relative h-space-02 w-full overflow-hidden bg-background-secondary', className)}
+                {...props}>
+                <ProgressPrimitive.Indicator
+                    className={cn('h-full w-full flex-1 transition-all', colors)}
+                    style={{
+                        transform: props?.dir === 'rtl' ? `translateX(${100 - (value || 0)}%)` : `translateX(-${100 - (value || 0)}%)`,
+                    }}
+                />
+            </ProgressPrimitive.Root>
+            {!!icon && (
+                <div
+                    className="absolute top-1/2 -translate-y-1/2 duration-200"
+                    style={{
+                        [props?.dir === 'rtl' ? 'right' : 'left']: `calc(${value}% + -12px)`,
+                    }}>
+                    {icon}
+                </div>
+            )}
+        </div>
     ),
 )
 Progress.displayName = ProgressPrimitive.Root.displayName
