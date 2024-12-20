d478c71649136c33909b31435ffd32a82b5855a5
mahmoud a.toumy <maabdullah.c@tetco.sa>
2024-12-21 00:01:10 +0200

    update exports

---
+++ src/Stores/index.ts
@@ -0,0 +1,2 @@
+export { AuthProvider } from './OicdAuth/authProvider'
+export * from './useLanguage'
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from './Components'
 export * from './Hooks'
+export * from './Lib'
+export * from './Stores'
