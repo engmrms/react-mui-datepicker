b2fd15564498b3b06bb8b9dd30f44f0fe5ca3ba6
mohammad <mrabah@tetco.sa>
2025-02-25 01:20:44 +0300

    accordion

--- src/Components/stories/surfaces/accordion.stories.tsx
+++ src/Components/stories/surfaces/accordion.stories.tsx
@@ -1,15 +1,55 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/no-unescaped-entities */
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Add, Minimize } from 'google-material-icons/outlined'
+import { Add, KeyboardArrowDown, Minimize } from 'google-material-icons/outlined'
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../../ui/accordion'
 
-function AccordionDemo(arg: any) {
+type WrapperProps = React.ComponentProps<typeof Accordion> & {
+    size?: 'default' | 'sm' | 'xs'
+    isLeading?: boolean
+}
+
+const AccordionWrapper = ({ size, isLeading, ...props }: WrapperProps) => {
+    // You can use size and isLeading here if needed
+    return <AccordionDemo {...props} />
+}
+
+function AccordionDemo(arg: WrapperProps) {
+    console.log(arg.size)
     return (
-        <Accordion {...arg} type="single" collapsible className="w-full">
+        <Accordion {...arg} className="w-full">
             <AccordionItem value="item-1">
-                <AccordionTrigger>
+                <AccordionTrigger size={arg.size} isLeading={arg?.isLeading}>
+                    Is it accessible?
+                    <KeyboardArrowDown className="transition-transform duration-500 ease-in-out group-data-[state=open]:rotate-180" aria-hidden />
+                </AccordionTrigger>
+                <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+                <AccordionTrigger size={arg.size} isLeading={arg?.isLeading}>
+                    Is it styled?
+                    <KeyboardArrowDown className="transition-transform duration-500 ease-in-out group-data-[state=open]:rotate-180" aria-hidden />
+                </AccordionTrigger>
+                <AccordionContent>Yes. It comes with default styles that matches the other components&apos; aesthetic.</AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+                <AccordionTrigger size={arg.size} isLeading={arg?.isLeading}>
+                    Is it animated?
+                    <KeyboardArrowDown className="transition-transform duration-500 ease-in-out group-data-[state=open]:rotate-180" aria-hidden />
+                </AccordionTrigger>
+                <AccordionContent>Yes. It's animated by default, but you can disable it if you prefer.</AccordionContent>
+            </AccordionItem>
+        </Accordion>
+    )
+}
+
+function AccordionCustomIcon(arg: WrapperProps) {
+    return (
+        <Accordion {...arg} className="w-full">
+            <AccordionItem value="item-1">
+                <AccordionTrigger size="xs">
                     Is it accessible?
                     <Add className="group-data-[state=open]:hidden" />
                     <Minimize className="group-data-[state=closed]:hidden" />
@@ -17,15 +57,17 @@ function AccordionDemo(arg: any) {
                 <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-2">
-                <AccordionTrigger>
-                    Is it styled? <Add className="group-data-[state=open]:hidden" />
+                <AccordionTrigger size="xs">
+                    Is it styled?
+                    <Add className="group-data-[state=open]:hidden" />
                     <Minimize className="group-data-[state=closed]:hidden" />
                 </AccordionTrigger>
                 <AccordionContent>Yes. It comes with default styles that matches the other components&apos; aesthetic.</AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
-                <AccordionTrigger>
-                    Is it animated? <Add className="group-data-[state=open]:hidden" />
+                <AccordionTrigger size="xs">
+                    Is it animated?
+                    <Add className="group-data-[state=open]:hidden" />
                     <Minimize className="group-data-[state=closed]:hidden" />
                 </AccordionTrigger>
                 <AccordionContent>Yes. It's animated by default, but you can disable it if you prefer.</AccordionContent>
@@ -34,11 +76,23 @@ function AccordionDemo(arg: any) {
     )
 }
 
-const meta: Meta<typeof Accordion> = {
+const meta: Meta<typeof AccordionWrapper> = {
     title: 'Design System/Surface/Accordion',
-    component: AccordionDemo,
+    component: AccordionWrapper,
     tags: ['autodocs'],
-    argTypes: {},
+
+    args: {
+        type: 'single',
+        collapsible: false,
+        disabled: false,
+        size: 'default',
+        isLeading: false,
+    },
+    argTypes: {
+        type: { control: 'radio', options: ['single', 'multiple'] },
+        size: { control: 'select', options: ['default', 'sm', 'xs'] },
+        collapsible: { description: "When type is 'single', allows closing content when clicking trigger for an open item." },
+    },
     parameters: {
         docs: {
             description: {
@@ -49,7 +103,13 @@ const meta: Meta<typeof Accordion> = {
 }
 
 export default meta
-type Story = StoryObj<typeof Accordion>
+
+type Story = StoryObj<typeof AccordionWrapper>
+
 export const Default: Story = {
-    render: AccordionDemo,
+    render: args => <AccordionDemo {...args} />,
+}
+
+export const CustomIcon: Story = {
+    render: AccordionCustomIcon,
 }
--- src/Components/ui/accordion.tsx
+++ src/Components/ui/accordion.tsx
@@ -10,26 +10,26 @@ const AccordionItem = React.forwardRef<
     React.ElementRef<typeof AccordionPrimitive.Item>,
     React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
 >(({ className, ...props }, ref) => (
-    <AccordionPrimitive.Item
-        ref={ref}
-        className={cn(
-            'flex flex-col gap-space-03 border-b border-b-border px-space-02 py-space-04 data-[state=closed]:hover:bg-card-hover',
-            className,
-        )}
-        {...props}
-    />
+    <AccordionPrimitive.Item ref={ref} className={cn('flex flex-col border-b border-b-border-neutral-primary', className)} {...props} />
 ))
 AccordionItem.displayName = 'AccordionItem'
 
 const AccordionTrigger = React.forwardRef<
     React.ElementRef<typeof AccordionPrimitive.Trigger>,
-    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
+    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger> & { size?: 'default' | 'sm' | 'xs'; isLeading?: boolean }
+>(({ className, children, size = 'default', isLeading = false, ...props }, ref) => (
     <AccordionPrimitive.Header className="flex">
         <AccordionPrimitive.Trigger
             ref={ref}
             className={cn(
-                'group    flex flex-1 items-center justify-between gap-space-03 text-right font-bold text-subtitle-02 text-primary transition-all disabled:text-disabled data-[state=closed]:text-foreground',
+                'group flex flex-1 items-center gap-space-04 border-2 border-transparent px-space-04  text-body-02 text-text-default transition-all hover:bg-button-background-neutral-hovered focus:border-border-black focus:bg-transparent focus:outline-none active:bg-button-background-neutral-pressed disabled:text-disabled-text-default-disabled [&>svg]:size-space-04',
+                {
+                    'py-space-04 font-bold': size === 'default',
+                    'py-space-03 font-semibold': size === 'sm',
+                    'py-space-02 font-semibold': size === 'xs',
+                    'justify-between ': !isLeading,
+                    'flex-row-reverse justify-end': isLeading,
+                },
                 className,
             )}
             {...props}>
@@ -45,9 +45,9 @@ const AccordionContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
     <AccordionPrimitive.Content
         ref={ref}
-        className="overflow-hidden text-body-02 text-card-foreground transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+        className="overflow-hidden text-body-02 text-text-paragraph-primary transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down data-[disabled]:text-disabled-text-default-disabled"
         {...props}>
-        <div className={cn('pb-4 pt-0', className)}>{children}</div>
+        <div className={cn('pb-space-05 pe-space-08 ps-space-04 pt-space-02', className)}>{children}</div>
     </AccordionPrimitive.Content>
 ))
 
