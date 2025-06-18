d19ac7e3517533c86300233bb0e01458fb8c2611
mahmoud a.toumy <maabdullah.c@tetco.sa>
2025-06-18 09:10:14 +0300

    init time picker component

---
+++ src/Components/stories/controls/timePicker.stories.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useArgs } from '@storybook/preview-api'
+import type { Meta, StoryObj } from '@storybook/react'
+
+import TimePicker from '../../ui/TimePicker'
+const meta: Meta<typeof TimePicker> = {
+    title: 'Design System/Controls/TimePicker',
+    component: TimePicker,
+    tags: ['autodocs'],
+}
+
+export default meta
+
+// /**
+//  * ##### This react time picke component let users select time values.
+//  */
+type Story = StoryObj<typeof TimePicker>
+
+// /**
+//  * # The Button component
+//  * Shows a button
+//  */
+export const Default: Story = {
+    args: {
+        lang: 'ar',
+        placeholder: '',
+        rounded: 'default',
+        value: '',
+        variant: 'default',
+        colors: 'default',
+        size: 'default',
+        ampm: true,
+    },
+    argTypes: {
+        value: { control: 'text' },
+    },
+    render: arg => {
+        const [storyArgs, updateArgs] = useArgs()
+        return (
+            <TimePicker
+                {...arg}
+                onChange={v => {
+                    if (v !== storyArgs.value) {
+                        updateArgs({ value: v })
+                    }
+                }}
+            />
+        )
+    },
+}
