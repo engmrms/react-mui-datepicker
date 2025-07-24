e1a6b8bbf1aa76206df1bd3ad506b610296453dd
mohammad <mrabah@tetco.sa>
2025-07-24 09:05:55 +0300

    fix filter render item

--- src/Components/Filter.tsx
+++ src/Components/Filter.tsx
@@ -59,17 +59,19 @@ interface FilterGroupProps extends React.HTMLAttributes<HTMLDivElement> {
     /** Icon for the filter button */
     filterIcon?: React.ReactNode
 }
+type FilterData = { value: string; label: string }
 
 interface FilterSelectProps {
     placeholder: string
     name: string
     multi?: boolean
-    data: { value: string; label: string; renderItem?: React.ReactNode }[]
+    data: FilterData[]
     disabled?: boolean
     isLoading?: boolean
     rounded?: 'default' | 'full'
     size?: 'default' | 'sm'
     defaultOpen?: boolean
+    renderItem?: (opt: FilterData) => React.ReactNode
 }
 
 interface MenuItemProps<T extends string | number = string> {
@@ -222,79 +224,81 @@ const FilterMobileView = ({
     )
 }
 
-const FilterSelect = React.memo(({ multi, data, placeholder, disabled, isLoading, rounded, size, name, defaultOpen }: FilterSelectProps) => {
-    const { value, upsert } = useFilterContext()
-    const mobileView = useMediaQuery('(max-width: 767px)')
-    const { dir } = useLanguage()
+const FilterSelect = React.memo(
+    ({ multi, data, placeholder, disabled, isLoading, rounded, size, name, defaultOpen, renderItem }: FilterSelectProps) => {
+        const { value, upsert } = useFilterContext()
+        const mobileView = useMediaQuery('(max-width: 767px)')
+        const { dir } = useLanguage()
 
-    if (mobileView) {
-        return (
-            <Collapsible dir={dir} defaultOpen={defaultOpen} className="border-b border-border-primary px-space-03 py-space-02">
-                <CollapsibleTrigger id="subcategoriesCollapsible" className=" flex w-full items-center  justify-between py-space-01 ps-space-02 ">
-                    <span className="text-body-01 font-semibold">{placeholder}</span>
-                    <div className="flex items-center  gap-space-01">
-                        {typeof value?.[name] === 'object' && !!value?.[name]?.length && (
-                            <span className="flex h-[24px] w-[24px] items-center justify-center rounded-full bg-inverted text-caption-01 text-inverted-foreground">
-                                {handleNumberDisplay(value?.[name]?.length || 0)}
-                            </span>
-                        )}
-                        {typeof value?.[name] === 'string' && !!value?.[name] && (
-                            <span className="flex h-[24px] w-[24px] items-center justify-center rounded-full bg-inverted text-caption-01 text-inverted-foreground">
-                                01
-                            </span>
-                        )}
-                        <Button variant={'text'} type="button" size={'icon-sm'} colors={'gray'}>
-                            <ExpandLess className=" transition-transform duration-300 group-data-[state=closed]:rotate-180" />
-                        </Button>
-                    </div>
-                </CollapsibleTrigger>
-                <CollapsibleContent>
-                    {multi ? (
-                        <FilterMobileMultipleSelect data={data} name={name} multi={multi} />
-                    ) : (
-                        <RadioGroup
-                            name={name}
-                            size="sm"
-                            dir={dir}
-                            value={typeof value?.[name] === 'string' ? value?.[name] : undefined}
-                            onValueChange={value => upsert({ name, selectedValue: value })}>
+        if (mobileView) {
+            return (
+                <Collapsible dir={dir} defaultOpen={defaultOpen} className="border-b border-border-primary px-space-03 py-space-02">
+                    <CollapsibleTrigger id="subcategoriesCollapsible" className=" flex w-full items-center  justify-between py-space-01 ps-space-02 ">
+                        <span className="text-body-01 font-semibold">{placeholder}</span>
+                        <div className="flex items-center  gap-space-01">
+                            {typeof value?.[name] === 'object' && !!value?.[name]?.length && (
+                                <span className="flex h-[24px] w-[24px] items-center justify-center rounded-full bg-inverted text-caption-01 text-inverted-foreground">
+                                    {handleNumberDisplay(value?.[name]?.length || 0)}
+                                </span>
+                            )}
+                            {typeof value?.[name] === 'string' && !!value?.[name] && (
+                                <span className="flex h-[24px] w-[24px] items-center justify-center rounded-full bg-inverted text-caption-01 text-inverted-foreground">
+                                    01
+                                </span>
+                            )}
+                            <Button variant={'text'} type="button" size={'icon-sm'} colors={'gray'}>
+                                <ExpandLess className=" transition-transform duration-300 group-data-[state=closed]:rotate-180" />
+                            </Button>
+                        </div>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                        {multi ? (
                             <FilterMobileMultipleSelect data={data} name={name} multi={multi} />
-                        </RadioGroup>
-                    )}
-                </CollapsibleContent>
-            </Collapsible>
-        )
-    }
+                        ) : (
+                            <RadioGroup
+                                name={name}
+                                size="sm"
+                                dir={dir}
+                                value={typeof value?.[name] === 'string' ? value?.[name] : undefined}
+                                onValueChange={value => upsert({ name, selectedValue: value })}>
+                                <FilterMobileMultipleSelect data={data} name={name} multi={multi} />
+                            </RadioGroup>
+                        )}
+                    </CollapsibleContent>
+                </Collapsible>
+            )
+        }
 
-    return multi ? (
-        <MultiSelect
-            options={data || []}
-            onChange={value => upsert({ name, selectedValue: value })}
-            selectedValues={(value?.[name] as string[]) || []}
-            dataTestId="muliSelect"
-            placeholder={placeholder}
-            disabled={disabled}
-            rounded={rounded}
-            size={size}
-            className="!w-max"
-        />
-    ) : (
-        <ComboboxControlNoForm
-            className="!w-max"
-            rounded={rounded}
-            options={data || []}
-            optionLabel="label"
-            optionValue="value"
-            renderItem={opt => opt?.renderItem}
-            placeholder={placeholder}
-            onChange={value => upsert({ name, selectedValue: value })}
-            value={(value?.[name] as string) || ''}
-            size={size}
-            disabled={disabled}
-            isLoading={isLoading}
-        />
-    )
-})
+        return multi ? (
+            <MultiSelect
+                options={data || []}
+                onChange={value => upsert({ name, selectedValue: value })}
+                selectedValues={(value?.[name] as string[]) || []}
+                dataTestId="muliSelect"
+                placeholder={placeholder}
+                disabled={disabled}
+                rounded={rounded}
+                size={size}
+                className="!w-max"
+            />
+        ) : (
+            <ComboboxControlNoForm
+                className="!w-max"
+                rounded={rounded}
+                options={data || []}
+                optionLabel="label"
+                optionValue="value"
+                renderItem={renderItem}
+                placeholder={placeholder}
+                onChange={value => upsert({ name, selectedValue: value })}
+                value={(value?.[name] as string) || ''}
+                size={size}
+                disabled={disabled}
+                isLoading={isLoading}
+            />
+        )
+    },
+)
 
 FilterSelect.displayName = 'FilterSelect'
 
--- src/Components/stories/controls/filter.stories.tsx
+++ src/Components/stories/controls/filter.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { FilterAlt } from 'google-material-icons/outlined'
-import { strings } from '../../../package'
+import { Stack, strings } from '../../../package'
 import { FilterGroup, FilterSelect } from '../../Filter'
 
 const meta: Meta<typeof FilterGroup> = {
@@ -46,6 +46,51 @@ export const Default: Story = {
                     value: i.toString(),
                     label: `label1${i}`,
                 }))}
+            />
+            <FilterSelect
+                name="test2"
+                multi
+                placeholder="select"
+                rounded="full"
+                data={Array.from({ length: 10 }, (_, i) => ({
+                    value: i.toString(),
+                    label: `multi${i}`,
+                }))}
+            />
+        </FilterGroup>
+    ),
+}
+
+export const RenderItem: Story = {
+    args: {
+        label: strings.Shared.Select,
+        resetButtonProps: { rounded: 'full', size: 'default' },
+        filterButtonProps: { rounded: 'full', size: 'default' },
+        resetButtonLabel: 'Reset',
+        filterIcon: <FilterAlt />,
+    },
+    render: arg => (
+        <FilterGroup
+            onValueChange={v => console.log('main', v)}
+            className="flex gap-space-03 "
+            onValueReset={() => console.log('first')}
+            resetButtonProps={{ rounded: 'full', size: 'default' }}
+            {...arg}>
+            <FilterSelect
+                defaultOpen
+                name="test"
+                rounded="full"
+                placeholder="select"
+                data={Array.from({ length: 10 }, (_, i) => ({
+                    value: i.toString(),
+                    label: `label1${i}`,
+                }))}
+                renderItem={opt => (
+                    <Stack className="w-full text-body-01" justifyContent={'between'}>
+                        <span>{opt.label}</span>
+                        <span>{opt.value}</span>
+                    </Stack>
+                )}
             />
             <FilterSelect
                 name="test2"
