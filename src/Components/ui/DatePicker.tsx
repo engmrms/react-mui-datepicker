be632bc33a1bc5c4e2c26e2bab75de2d1a9250f5
Mohammad Saleh <41836876+engmrms@users.noreply.github.com>
2025-10-07 12:52:15 +0300

    fix: remove .prettierrc configuration file, update package version to 3.0.1 (#4)
    Co-authored-by: mohammad <mrabah@tetco.sa>

--- src/Components/ui/DatePicker.tsx
+++ src/Components/ui/DatePicker.tsx
@@ -1,15 +1,15 @@
-import { DateCalendarProps } from '@mui/x-date-pickers'
-import { cva, VariantProps } from 'class-variance-authority'
-import { DateRange } from 'google-material-icons/outlined'
-import moment from 'moment-hijri'
-import { useState } from 'react'
-import { cn } from '../../Lib/utils'
-import { Calendar } from './calendar'
-import { Popover, PopoverContent, PopoverTrigger } from './popover'
+import { DateCalendarProps } from "@mui/x-date-pickers";
+import { cva, VariantProps } from "class-variance-authority";
+import { DateRange } from "google-material-icons/outlined";
+import moment from "moment-hijri";
+import { useState } from "react";
+import { cn } from "../../Lib/utils";
+import { Calendar } from "./calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 
-import { PickerValue } from '@mui/x-date-pickers/internals'
-import 'moment/dist/locale/ar-ly'
-import { Switch } from './switch'
+import { PickerValue } from "@mui/x-date-pickers/internals";
+import "moment/dist/locale/ar-ly";
+import { Switch } from "./switch";
 
 const datePickerVariants = cva(
   ` placeholder:text-form-field-text-placeholder data-[placeholder]:text-form-field-text-placeholder  text-form-field-text-filled after:bg-form-field-border-pressed group relative
@@ -21,102 +21,124 @@ const datePickerVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-form-field-background-darker  enabled:hover:border-form-field-border-default',
+        default:
+          "bg-form-field-background-darker  enabled:hover:border-form-field-border-default",
         outline:
-          'bg-form-field-background-default enabled:hover:border-form-field-border-hovered border border-form-field-border-default disabled:border-border-disabled',
-        lighter: 'bg-form-field-background-lighter  enabled:hover:border-form-field-border-default',
+          "bg-form-field-background-default enabled:hover:border-form-field-border-hovered border border-form-field-border-default disabled:border-border-disabled",
+        lighter:
+          "bg-form-field-background-lighter  enabled:hover:border-form-field-border-default",
       },
       rounded: {
-        default: 'rounded',
-        full: 'rounded-4',
+        default: "rounded",
+        full: "rounded-4",
       },
       colors: {
-        default: 'hover:enabled:border',
-        success: 'border-success',
-        destructive: 'border-form-field-border-error ',
+        default: "hover:enabled:border",
+        success: "border-success",
+        destructive: "border-form-field-border-error ",
       },
       size: {
-        default: 'py-space-02 text-body-02 h-[40px]',
-        sm: 'py-space-01 text-body-01 h-[32px]',
+        default: "py-space-02 text-body-02 h-[40px]",
+        sm: "py-space-01 text-body-01 h-[32px]",
       },
     },
     defaultVariants: {
-      variant: 'outline',
-      rounded: 'default',
-      colors: 'default',
-      size: 'default',
+      variant: "outline",
+      rounded: "default",
+      colors: "default",
+      size: "default",
     },
   },
-)
+);
 
 interface Props extends VariantProps<typeof datePickerVariants> {
-  lang?: 'ar' | 'en'
-  placeholder?: string | null
-  value: Date | string | null | undefined | PickerValue
-  onChange: (value: string) => void
-  defaultToToday?: boolean
-  className?: string
-  defaultCalendar?: 'hijri' | 'gregorian'
-  switchLabel?: string
-  showSwitch?: boolean
-  dir?: 'ltr' | 'rtl'
-  momentFormat?: (lang?: 'ar' | 'en', calendar?: 'hijri' | 'gregorian') => string
+  lang?: "ar" | "en";
+  placeholder?: string | null;
+  value: Date | string | null | undefined | PickerValue;
+  onChange: (value: string) => void;
+  defaultToToday?: boolean;
+  className?: string;
+  defaultCalendar?: "hijri" | "gregorian";
+  switchLabel?: string;
+  showSwitch?: boolean;
+  dir?: "ltr" | "rtl";
+  momentFormat?: (
+    lang?: "ar" | "en",
+    calendar?: "hijri" | "gregorian",
+  ) => string;
 }
 
 export const DatePicker = ({
   placeholder,
   value,
   onChange,
-  lang = 'en',
+  lang = "en",
   rounded,
   defaultToToday,
   variant,
   className,
   colors,
   size,
-  defaultCalendar = 'gregorian',
-  switchLabel = 'Hijri Calendar',
+  defaultCalendar = "gregorian",
+  switchLabel = "Hijri Calendar",
   showSwitch,
-  dir = 'ltr',
+  dir = "ltr",
   momentFormat,
   ...rest
-}: Props & Omit<DateCalendarProps, 'value'>) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const [toggleCalendar, setToggleCalendar] = useState<'hijri' | 'gregorian'>(defaultCalendar)
+}: Props & Omit<DateCalendarProps, "value">) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [toggleCalendar, setToggleCalendar] = useState<"hijri" | "gregorian">(
+    defaultCalendar,
+  );
   const dateValue =
-    value && moment(value).isValid() ? moment(value) : defaultToToday ? moment() : null
+    value && moment(value).isValid()
+      ? moment(value)
+      : defaultToToday
+        ? moment()
+        : null;
 
-  const dateFormate = lang === 'ar' ? 'YYYY/MM/DD' : 'DD/MM/YYYY'
-  const localeToUse = lang === 'ar' ? 'ar-ly' : 'en'
-  moment.locale(localeToUse)
+  const dateFormate = lang === "ar" ? "YYYY/MM/DD" : "DD/MM/YYYY";
+  const localeToUse = lang === "ar" ? "ar-ly" : "en";
+  moment.locale(localeToUse);
   const handleDateChange = (date: PickerValue) => {
-    setIsOpen(false)
-    if (!date) return
-    onChange(date?.format())
-  }
+    setIsOpen(false);
+    if (!date) return;
+    onChange(date?.format());
+  };
 
   return (
-    <Popover open={isOpen} onOpenChange={setIsOpen}>
+    <Popover open={isOpen} onOpenChange={setIsOpen} modal={true}>
       <PopoverTrigger asChild>
         <button
           id="date"
           role="date"
-          className={cn(datePickerVariants({ variant, colors, size, rounded }), className)}
-          disabled={rest.disabled}>
+          className={cn(
+            datePickerVariants({ variant, colors, size, rounded }),
+            className,
+          )}
+          disabled={rest.disabled}
+        >
           {dateValue ? (
-            dateValue.format(momentFormat?.(lang, toggleCalendar) || dateFormate)
+            dateValue.format(
+              momentFormat?.(lang, toggleCalendar) || dateFormate,
+            )
           ) : (
             <span className="text-body-01 text-foreground-secondary">
               {placeholder ||
                 (defaultToToday
-                  ? moment().format(momentFormat?.(lang, toggleCalendar) || dateFormate)
-                  : '')}
+                  ? moment().format(
+                      momentFormat?.(lang, toggleCalendar) || dateFormate,
+                    )
+                  : "")}
             </span>
           )}
           <DateRange className="size-[20px]  shrink-0 " />
         </button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto rounded-2 border-border-secondary p-0 shadow-2xl" dir={dir}>
+      <PopoverContent
+        className="w-auto rounded-2 border-border-secondary p-0 shadow-2xl"
+        dir={dir}
+      >
         <>
           {showSwitch && (
             <div className=" p-space-04">
@@ -124,7 +146,9 @@ export const DatePicker = ({
                 <label htmlFor="">{switchLabel}</label>
                 <Switch
                   onCheckedChange={() =>
-                    setToggleCalendar(toggleCalendar === 'hijri' ? 'gregorian' : 'hijri')
+                    setToggleCalendar(
+                      toggleCalendar === "hijri" ? "gregorian" : "hijri",
+                    )
                   }
                   dir={dir}
                   checked={toggleCalendar !== defaultCalendar}
@@ -134,7 +158,7 @@ export const DatePicker = ({
           )}
           <Calendar
             dir={dir}
-            ishijri={toggleCalendar === 'hijri'}
+            ishijri={toggleCalendar === "hijri"}
             value={dateValue ?? moment()}
             lang={lang}
             onChange={handleDateChange}
@@ -143,7 +167,7 @@ export const DatePicker = ({
         </>
       </PopoverContent>
     </Popover>
-  )
-}
+  );
+};
 
-export default DatePicker
+export default DatePicker;
